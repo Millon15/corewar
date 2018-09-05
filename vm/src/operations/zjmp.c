f97423e31a298a5213b79bd27537b274a38700ea
ArtemisKS <Artemkupriyanets@yahoo.com>
2018-09-05 23:21:40 +0300

    some shit done

--- vm/src/operations/zjmp.c
+++ vm/src/operations/zjmp.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   zjmp.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vbrazas <vbrazas@student.unit.ua>          +#+  +:+       +#+        */
+/*   By: akupriia <akupriia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/13 19:49:34 by vbrazas           #+#    #+#             */
-/*   Updated: 2018/09/05 16:41:19 by vbrazas          ###   ########.fr       */
+/*   Updated: 2018/09/05 23:21:19 by akupriia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,34 @@
 
 static void	jump_car(t_car *self, t_vm *v, int val)
 {
-	self->pc = (v->arena + (val) % MEM_SIZE);
+	if (self->carry == true)
+	{
+		self->pc = (v->arena + (val) % MEM_SIZE);
+		if (v->args.verbose_value & 4)
+			ft_printf("P\t%d | zjmp %d OK\n", self->id, val);
+	}
+	else
+		if (v->args.verbose_value & 4)
+			ft_printf("P\t%d | zjmp %d FAILED\n", self->id, val);
 }
 
 static void	move_neg_val(t_car *self, t_vm *v, int val)
 {
-	self->pc = (v->arena + (self->pc - v->arena + val) % MEM_SIZE);	
+	if (self->carry == true)
+	{
+		self->pc = (v->arena + (self->pc - v->arena + val) % MEM_SIZE);
+		if (v->args.verbose_value & 4)
+			ft_printf("P\t%d | zjmp %d OK\n", self->id, val);
+	}
+	else
+		if (v->args.verbose_value & 4)
+			ft_printf("P\t%d | zjmp %d FAILED\n", self->id, val);
 }
 
 void		zjmp(t_car *self, t_vm *v)
 {
 	int					num;
 	
-	if (self->carry == false)
-	{
-		move_pc(self, v, self->pc_padding);
-		self->pc_padding = 0;
-		return ;
-	}
 	// ft_printf("I.cycle: %d | self->arg_val[0]: %d\n", I.cur_cycle, self->arg_val[0]);
 	if (self->arg_val[0] >= IDX_MOD)
 	{
@@ -41,7 +51,7 @@ void		zjmp(t_car *self, t_vm *v)
 		// ft_printf("I.cycle: %d | self->arg_val[0]: %d, mod(self->arg_val[0]): %d | self->pc - v->arena %d\n", I.cur_cycle, self->arg_val[0], mod(self->arg_val[0]), self->pc - v->arena);
 		// if (self->arg_val[0] > &v->arena[((int)v->player[UINT_MAX - WHOM(self)].prog_size)] - self->pc)
 		if (mod(self->arg_val[0]) > self->pc - v->arena)
-			move_pc(self, v, self->pc_padding);
+			jump_car(self, v, MEM_SIZE - (mod(self->arg_val[0]) - self->pc - v->arena))
 			// jump_car(self, v, MEM_SIZE - mod(self->arg_val[0]) - (self->pc - v->arena));
 		else
 		{
@@ -54,8 +64,8 @@ void		zjmp(t_car *self, t_vm *v)
 	}
 	else
 		if (self->arg_val[0] > MEM_SIZE - (self->pc - v->arena))
-			jump_car(self, v, self->arg_val[0]);
+			jump_car(self, v, self->arg_val[0] - (MEM_SIZE - (self->pc - v->arena)));
 		else
-			move_pc(self, v, self->arg_val[0]);
+			jump_car(self, v, self->arg_val[0]);
 	self->pc_padding = 0;
 }
