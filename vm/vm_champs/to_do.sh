400cd85a0b55cc6b5542c09d049b9c3c066176f7
ArtemisKS <Artemkupriyanets@yahoo.com>
2019-02-07 16:31:03 +0200

    readme finally added, .cor files present as well

--- vm/vm_champs/to_do.sh
+++
@@ -1,2 +0,0 @@
-diff /tmp/log_26294 /tmp/log__26294 > /tmp/diff_26294
-#  ./champs/championships/2017/agregoir/Kittystrophic.cor ./champs/barriere.cor ./champs/championships/2014/kelickel/Varimathras.cor ./champs/42.cor
